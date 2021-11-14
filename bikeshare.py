29dc8c99dd861c294553ba3307a605815b9258a9
a4ayub <robert.ayub@gmail.com>
2021-11-14 08:48:56 +0300

    feat: Updated the message prompt for users. Added a note thanking the users

--- bikeshare.py
+++ bikeshare.py
@@ -248,7 +248,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nThank you for using the tool, would you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
